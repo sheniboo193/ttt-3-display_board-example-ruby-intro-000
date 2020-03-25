998e8ad13bacdae84e021f7b2befab91f5bd7408
Faizah Tasnim <ft.2129@gmail.com>
2020-03-25 02:31:00 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -9,13 +9,13 @@ describe '/lib/display_board.rb' do
     it 'represents a cell as a string with 3 spaces' do
       output = capture_puts{ display_board }
 
-      expect(output).to include("   ")
+      expect(output).to "include("   ")"
     end
-    
+
     it 'separates cells with a | character' do
       output = capture_puts{ display_board }
 
-      expect(output).to include("   |   ")      
+      expect(output).to include("   |   ")
     end
 
     it 'prints an 3 cell row' do
